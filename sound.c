a1fee185b6e945e56243341453c4225c0f809771
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2023-08-05 17:49:05 +0200

    Remove buggy sound code

--- sound.c
+++ sound.c
@@ -33,167 +33,40 @@ int num_of_loaded_sounds = 0;
 
 int sb_init(void)
 {
-    asm {
-		mov al,1
-		mov dx,SB_BASE_ADDRESS
-		add dx,6
-		out dx,al
-		in al,dx
-		in al,dx
-		in al,dx
-		in al,dx
-		xor al,al
-		out dx,al
-		add dx,4
-		mov cx,100
-    }
-sb_test:
-    asm {
-		in al,dx
-		cmp al,0aah
-		je sb_present
-		loop sb_test
-    }
-
-    return 0;
-sb_present:
     return 1;
 }
 
 void sb_dsp_out(unsigned char value)
 {
-    asm {
-		mov dx,SB_BASE_ADDRESS
-		add dx,0ch
-    }
-dsp_out_test:
-    asm {
-		in al,dx
-		test al,80h
-		jne dsp_out_test
-		mov al,value
-		out dx,al
-    }
 }
 
 void interrupt dma_int(void)
 {
-    end_of_sound = 1;
-
-    asm {
-		mov dx,SB_BASE_ADDRESS
-		add dx,0eh
-		in al,dx
-		sub dx,2
-    }
-dma_sb_reset:
-    asm {
-		in al,dx
-		or al,al
-		js dma_sb_reset
-
-		mov al,20h
-		out 20h,al
-    }
 }
 
 void enable_dma_int(void)
 {
-    old_dma_int = getvect(DMA_INT);
-    setvect(DMA_INT, dma_int);
-
-    asm {
-		in al,21h
-		and al,0dfh
-		out 21h,al
-    }
 }
 
 void disable_dma_int(void)
 {
-    setvect(DMA_INT, old_dma_int);
-
-    asm {
-		in al,21h
-		or al,20h
-		out 21h,al
-    }
 }
 
 void convert_address(unsigned segment, unsigned off1,
     unsigned char* page, unsigned* off2)
 {
-    asm {
-		mov dx,segment
-		mov ax,off1
-		mov bl,dh
-		shr bl,4
-		shl dx,4
-		add ax,dx
-		adc bl,0
-		mov dh,bl
-    }
-
-    *page = _DH;
-    *off2 = _AX;
 }
 
 void set_dma(unsigned char page, unsigned off, unsigned lenght)
 {
-    asm {
-		mov al,4
-		add al,DMA_CHANNEL
-		out 0ah,al
-
-		xor al,al
-		out 0ch,al
-
-		mov al,DMA_MODE
-		add al,DMA_CHANNEL
-		out 0bh,al
-
-		mov dx,DMA_OFFSET_PORT
-		mov ax,off
-		out dx,al
-		mov al,ah
-		out dx,al
-		inc dx
-		mov ax,lenght
-		out dx,al
-		mov al,ah
-		out dx,al
-
-		mov dx,DMA_PAGE_PORT
-		mov al,page
-		out dx,al
-
-		mov al,DMA_CHANNEL
-		out 0ah,al
-    }
 }
 
 void set_freq(unsigned freq)
 {
-    sb_dsp_out(0x40);
-    sb_dsp_out(256 - 1000000 / freq);
 }
 
 void play_sound(SOUND* s)
 {
-    unsigned char page;
-    unsigned off;
-
-    if (!end_of_sound)
-        return;
-
-    convert_address(FP_SEG(s->buffer), FP_OFF(s->buffer), &page, &off);
-    set_dma(page, off, s->lenght);
-
-    sb_dsp_out(0x14);
-    sb_dsp_out(s->lenght & 255);
-    sb_dsp_out(s->lenght >> 8);
-
-    end_of_sound = 0;
 }
 
 unsigned get_file_size(char* filename)
